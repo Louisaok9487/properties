b53e494e62f7d10f1006cb007d83fd9dcd25ddcf
Louis <corridorsorigamiwallet@gmail.com>
2025-07-31 14:27:51 +1200

    Update build.js

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Define the path to your HTML file
 // IMPORTANT: Make sure this matches the name of your HTML file.
-const htmlFilePath = path.join(__dirname, 'rental-return-app.html'); 
+const htmlFilePath = path.join(__dirname, 'index.html'); // This line is updated to 'index.html'
 const placeholder = '__SUPABASE_ANON_KEY_PLACEHOLDER__';
 // This is the environment variable Netlify will provide
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY; 
